2e058767193185772dd6a032aa391fd241dfea7b
FlorianThiers <florthiers@gmail.com>
2025-06-15 16:59:30 +0200

    minits uit env gehaald

--- backend/app/security/auth.py
+++ backend/app/security/auth.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = 600
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")  # of jouw login endpoint
 
